45be4ace646b40401ac7c213820c9c25ca4e6d36
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-05-15 14:31:14 +0300

    add a divide function to the js script file

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,9 @@ var add = function(number1, number2) {
     var number2 = parseInt(prompt("enter another number:"));
     alert(multiply(number1, number2));
     
+    var divide = function(number1, number2) {
+        return number1/ number2
+        }
+        var number1 = parseInt(prompt("enter a number:"));
+        var number2 = parseInt(prompt("enter another number:"))
+        alert(divide(number1, number2));
