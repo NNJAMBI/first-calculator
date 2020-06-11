2f507df31ddde26e4fdc9d789e66c835603fffef
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-06-11 14:13:42 +0300

    add changes made to the js and html forms

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,13 @@ $(document).ready(function() {
     $("#output").text(result);
   });
 });
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+});
